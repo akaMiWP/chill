38d6b2560d504c92fa4f4b9bed3a8ac1a9cea78c
akaMiWP <bangkokappdevelopment@gmail.com>
2025-07-05 08:06:30 +0700

    feat: added UI for Meditation page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,49 @@
-import { Text, View } from "react-native";
+import { Image, Pressable, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
+    <View className="flex-1">
+      <ScrollView className="bg-white p-4">
+        <View className="flex-row">
+          <View className="flex-col gap-2 max-w-[70%] p-4">
+            <Text className="text-[#9E8047] font-semibold">
+              5-minute Breathing
+            </Text>
+            <Text className="font-bold">Quick Calm</Text>
+            <Text className="text-[#9E8047] font-semibold">
+              Find peace in a short session.
+            </Text>
+            <Pressable className="bg-[#F5F0E5] w-[84] h-[32] flex justify-center rounded-2xl mt-4">
+              <Text className="font-semibold text-center">Start</Text>
+            </Pressable>
+          </View>
+
+          <Image
+            source={require("../../assets/images/img-focused-flow.png")}
+            className="ml-auto"
+          />
+        </View>
+
+        <View className="flex-row justify-end">
+          <View className="flex-col gap-2 max-w-[70%] p-4">
+            <Text className="text-[#9E8047] font-semibold">
+              25-minute Pomodoro
+            </Text>
+            <Text className="font-bold">Focused Flow</Text>
+            <Text className="text-[#9E8047] font-semibold">
+              Deep work with mindful breaks.
+            </Text>
+            <Pressable className="bg-[#F5F0E5] w-[84] h-[32] flex justify-center rounded-2xl mt-4">
+              <Text className="font-semibold text-center">Start</Text>
+            </Pressable>
+          </View>
+
+          <Image
+            source={require("../../assets/images/img-quick-calm.png")}
+            className="ml-auto"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
