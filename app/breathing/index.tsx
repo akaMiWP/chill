d2284691414d7276bc832566f9217b87c6c55884
akaMiWP <bangkokappdevelopment@gmail.com>
2025-07-12 19:48:59 +0700

    feat: added start, pause and reset functionalities

--- app/breathing/index.tsx
+++ app/breathing/index.tsx
@@ -1,23 +1,29 @@
 // app/breathing/index.tsx
 import { Stack } from "expo-router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 
-const Display = () => {
+const Display = ({
+  minutes,
+  seconds,
+}: {
+  minutes: number;
+  seconds: number;
+}) => {
   return (
     <View className="flex-row py-6 gap-4">
       <View className="flex-1 items-center gap-4">
         <View className="bg-[#F2EDE8] h-[56px] justify-center items-center w-full rounded-xl">
-          <Text className="text-center font-bold">14</Text>
+          <Text className="text-center font-bold">{minutes}</Text>
         </View>
-        <View className="h-[24px justify-center items-center w-full">
+        <View className="h-[24px] justify-center items-center w-full">
           <Text className="text-center">Minutes</Text>
         </View>
       </View>
 
       <View className="flex-1 items-center gap-4">
         <View className="bg-[#F2EDE8] h-[56px] justify-center items-center w-full rounded-xl">
-          <Text className="text-center font-bold">59</Text>
+          <Text className="text-center font-bold">{seconds}</Text>
         </View>
         <View className="h-[24px] justify-center items-center w-full">
           <Text className="text-center">Seconds</Text>
@@ -60,6 +66,39 @@ const ActionButtons = ({
 
 export default function BreathingScreen() {
   const [hasStarted, setHasStarted] = useState<boolean>(false);
+  const [reset, setReset] = useState<boolean>(false);
+  const [timeLeft, setTimeLeft] = useState<number>(15 * 60);
+
+  useEffect(() => {
+    let interval: number;
+
+    if (hasStarted) {
+      interval = setInterval(() => {
+        setTimeLeft((prevTime) => {
+          if (prevTime <= 1) {
+            clearInterval(interval);
+            resetState();
+            return 0;
+          }
+          return prevTime - 1;
+        });
+      }, 1000);
+    }
+
+    if (reset) {
+      setReset(false);
+      setTimeLeft(15 * 60);
+    }
+
+    return () => clearInterval(interval);
+  }, [hasStarted, reset]);
+
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
+  const resetState = () => {
+    setHasStarted(false);
+    setReset(true);
+  };
 
   return (
     <>
@@ -70,11 +109,11 @@ export default function BreathingScreen() {
         }}
       />
       <View className="flex-1 bg-white px-4">
-        <Display />
+        <Display minutes={minutes} seconds={seconds} />
         <ActionButtons
           hasStarted={hasStarted}
           onPress={setHasStarted}
-          onReset={() => setHasStarted(false)}
+          onReset={resetState}
         />
       </View>
     </>
